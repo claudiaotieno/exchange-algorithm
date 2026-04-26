980df5da14dec04b60100b8c017504a6008e7a94
claudiaotieno <claudiaadhiambo72@gmail.com>
2026-04-26 09:51:40 +0300

    Final Commit: Enhance Exchange Sort with dynamic allocation and metrics

--- Main.cpp
+++ Main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
-#include <cstdlib>
+#include <cstdlib>   
+#include <ctime>     
+
 using namespace std;
 
+// Exchange Sort Function
 void exchangeSort(int arr[], int n, long long &comparisons, long long &swaps) {
     for(int i = 0; i < n - 1; i++) {
         for(int j = i + 1; j < n; j++) {
@@ -24,18 +27,43 @@ int main() {
     cout << "Enter number of elements: ";
     cin >> n;
 
+    // Dynamic allocation for large datasets
     int* arr = new int[n];
 
+    // Seed random generator
+    srand(time(0));
+
+    // Generate random numbers
     for(int i = 0; i < n; i++) {
         arr[i] = rand() % 100000;
     }
+   
+    if(n <= 20) {
+        cout << "\nOriginal array:\n";
+        for(int i = 0; i < n; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+    }
 
+    // Perform Exchange Sort
     exchangeSort(arr, n, comparisons, swaps);
+   
+    if(n <= 20) {
+        cout << "\nSorted array:\n";
+        for(int i = 0; i < n; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+    }
 
-    cout << "Sorting complete.\n";
-    cout << "Comparisons: " << comparisons << endl;
-    cout << "Swaps: " << swaps << endl;
+    // Output performance metrics
+    cout << "\nSorting complete.\n";
+    cout << "Total Comparisons: " << comparisons << endl;
+    cout << "Total Swaps: " << swaps << endl;
 
+    // Free memory
     delete[] arr;
+
     return 0;
 }
